8e1555b5b9a1b5918ddcb7173841eeab6dfcb968
ClaudiaAlfieri <claudia.jorge86@gmail.com>
2025-06-29 23:47:18 +0100

    Routes new

--- routes/web.php
+++ routes/web.php
@@ -5,6 +5,10 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+Route::get('/comoFunciona', function () {
+    return view('ComoFunciona');
+});
 Route::get('/minhasReceitas', function () {
     return view('MinhasReceitas');
 });
+
