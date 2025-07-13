8ebd20431d9eb49d2236b5aedb4cd674d59b0010
ClaudiaAlfieri <claudia.jorge86@gmail.com>
2025-07-13 23:58:43 +0100

    Minhas receitas

--- resources/views/minhasReceitas.blade.php
+++ resources/views/minhasReceitas.blade.php
@@ -1,1 +1,16 @@
 @extends('layout')
+<body>
+    <main>
+        <section class="hero mt-10" id="inicio">
+            <div class="container">
+                <div class="hero-content">
+                    <h1>Transforme ingredientes em receitas incríveis</h1>
+                    <p>Descubra o que cozinhar com os ingredientes que você tem em casa. Nossa IA cria receitas
+                        personalizadas para você!</p>
+                </div>
+            </div>
+        </section>
+
+
+    </main>
+</body>
